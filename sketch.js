97ba8a733efab6c0f08abec4a6937eb65f3163d6
Minh Giang Do <mdo2@saic.edu>
2023-03-17 02:25:33 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,8 @@ let bg;
 let r1 = 0;
 let r2 = 0;
 let r3 = 0;
-
+let dup = 20;
+let shapeDup = 2;
 
 let sketch = function(p) {
     p.preload = function() {
@@ -87,29 +88,51 @@ let sketch = function(p) {
       let size = p.map(d, 0, 1, 1, 200);
       //console.log(d);
       
-      p.push();
-      p.translate(x1, y1);
-      p.rotate(r1);
-      p.image(eyeL, 0, 0, size*15 +20, size*15 +20);
-      p.pop();
+      // p.push();
+      // p.translate(x1, y1);
+      // p.rotate(r1);
+      // p.image(eyeL, 0, 0, size*15 +20, size*15 +20);
+      // p.pop();
       
-      p.push();
-      p.translate(x2, y2);
-      p.rotate(r2);
-      p.image(eyeR, 0, 0, size*15, size*15);
-      p.pop();
+      // p.push();
+      // p.translate(x2, y2);
+      // p.rotate(r2);
+      // p.image(eyeR, 0, 0, size*15, size*15);
+      // p.pop();
 
-      p.push();
-      p.translate(x3, y3);
-      p.rotate(r3);
-      p.image(mouth, 500, 0, size*15, size*15);
-      p.pop();
+      // p.push();
+      // p.translate(x3, y3);
+      // p.rotate(r3);
+      // p.image(mouth, 500, 0, size*15, size*15);
+      // p.pop();
 
-      for(let xx = 0; xx ++; x<4){
+      for(let xx1 = 0; xx1 < size*3; xx1 +=size){
         p.push();
-        p.translate(x3+200, y3);
+      p.translate(x1+xx1, y1+xx1);
+      p.rotate(r1);
+      p.image(eyeL, 0, 0, size*dup +20, size*dup +20);
+      p.pop();
+      }
+
+      for(let xx2 = 0; xx2 < size*3; xx2 +=size){
+        p.push();
+        p.translate(x2+xx2, y2+xx2);
+        p.rotate(r2);
+        p.image(eyeR, 0, 0, size*dup, size*dup);
+        p.pop();
+      }
+      
+      // if (shapeDup >1){
+      //   shapeDup +=1;
+      // }else{
+      //   shapeDup -=1;
+      // }
+      
+      for(let xx = 0; xx < size*shapeDup; xx +=size){
+        p.push();
+        p.translate(x3+xx, y3+xx);
         p.rotate(r3);
-        p.image(mouth, 0, 0, size*15, size*15);
+        p.image(mouth, 0, 0, size*dup, size*dup);
         p.pop();
       }
       
